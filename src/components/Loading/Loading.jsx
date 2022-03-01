029e872b86b41fbae7e999b6dcb49d1f48373eb3
Raluca Nicola <RNicola@esri.com>
2022-03-01 10:50:27 +0100

    remove loading star animations

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,8 +3,6 @@ import * as styles from './Loading.module.css';
 export function Loading() {
   return (
     <div className={styles.loadingPanel}>
-      <div className={styles.stars}></div>
-      <div className={styles.twinkling}></div>
       <img src='./assets/loading.png'></img>
       <p>Loading and computing locations for thousands of satellites...</p>
     </div>
