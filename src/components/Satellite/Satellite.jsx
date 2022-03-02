9cf25282f21289be109026059674dca9666eb6ad
Raluca Nicola <RNicola@esri.com>
2022-03-02 20:35:04 +0100

    Add information for each satellite

--- src/components/Satellite/Satellite.jsx
+++ src/components/Satellite/Satellite.jsx
@@ -4,6 +4,21 @@ import appStore from '../../stores/AppStore';
 import { observer } from 'mobx-react';
 import { BackButton } from '../BackButton';
 
+const ListItem = ({ field, value }) => {
+  return (
+    <>
+      {value ? (
+        <>
+          <p className={styles.itemTitle}>{field.toUpperCase()}</p>
+          <p className={styles.itemValue}>{value}</p>
+        </>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
 export const Satellite = observer(() => {
   let params = useParams();
 
@@ -19,8 +34,18 @@ export const Satellite = observer(() => {
       {attr ? (
         <>
           <h2>{attr.official_name}</h2>
-          <p>NORAD: {attr.norad}</p>
-          <p>Operator: {attr.operator}</p>
+          <ListItem field='Purpose' value={attr.purpose} />
+          <ListItem field='Operator / Owner' value={attr.operator} />
+          <ListItem field='Country' value={attr.country_operator} />
+          <ListItem field='Launch date' value={new Date(attr.launch_date).toLocaleDateString('en-US')} />
+          <ListItem field='Launch location' value={attr.launch_site} />
+          <ListItem field='Launch vehicle' value={attr.launch_vehicle} />
+          <ListItem field='Orbit type' value={attr.orbit_class} />
+          <ListItem field='Perigee' value={attr.perigee} />
+          <ListItem field='Apogee' value={attr.apogee} />
+          <ListItem field='Eccentricity' value={attr.exccentricity} />
+          <ListItem field='Inclination' value={attr.inclination} />
+          <ListItem field='Period' value={attr.period} />
         </>
       ) : (
         <p>Loading satellite information...</p>
--- src/components/Stories/Stories.jsx
+++ src/components/Stories/Stories.jsx
@@ -11,18 +11,6 @@ export function Stories() {
         <StoryCard link='satellite-number' image='./assets/satellite-2.jpg'>
           How many satellites are out there?
         </StoryCard>
-        <StoryCard link='satellite-internet' image='./assets/satellite-2.jpg'>
-          Internet for everyone, everywhere?
-        </StoryCard>
-        <StoryCard link='coming-soon' image='./assets/satellite-2.jpg'>
-          A space junk crisis?
-        </StoryCard>
-        <StoryCard link='coming-soon' image='./assets/satellite-2.jpg'>
-          Who owns the satellites?
-        </StoryCard>
-        <StoryCard link='coming-soon' image='./assets/satellite-2.jpg'>
-          The International Space Station
-        </StoryCard>
         <StoryCard link='search' image='./assets/satellite-2.jpg'>
           Search for satellites
         </StoryCard>
