29ef748a891a39f441bcb3de4f645113aabdf729
Raluca Nicola <RNicola@esri.com>
2022-03-08 20:30:24 +0100

    Improve page not foound

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -6,11 +6,13 @@ export function PageNotFound() {
     <div className={styles.wrapper}>
       <div className={styles.stars}></div>
       <div className={styles.twinkling}></div>
-      <img src='./assets/astronaut-notfound.png' />
-      <p>
-        Hey there! You went way beyond our galaxy and we can't find what you're looking for. But we can take you back to
-        the <Link to='/'>home page</Link>.
-      </p>
+      <div className={styles.textWrapper}>
+        <img src='./assets/astronaut-notfound.png' />
+        <p>
+          Hey there! You went way beyond our galaxy and we can't find what you're looking for. But we can take you back
+          to the <Link to='/'>home page</Link>.
+        </p>
+      </div>
     </div>
   );
 }
