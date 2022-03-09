05cecca58cee4b76fd5b49981abc23ba14ab4f39
Raluca Nicola <RNicola@esri.com>
2022-03-09 18:07:22 +0100

    Add loading gif

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -3,7 +3,7 @@ import * as styles from './Loading.module.css';
 export function Loading() {
   return (
     <div className={styles.loadingPanel}>
-      <img src='./assets/loading.png'></img>
+      <img src='./assets/loading.gif'></img>
       <p>Loading and computing locations for thousands of satellites...</p>
     </div>
   );
