377a240db027c5e329a5cedcdcb01fb6c764c82e
Raluca Nicola <RNicola@esri.com>
2022-03-30 15:24:35 +0200

    update config for the production build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => {
-  const base = command === 'build' ? 'https://apl-apps4.s3.us-west-2.amazonaws.com/satellite-explorer/' : './';
-  // const base = command === 'build' ? 'https://10.195.20.28/demo-apps/satellite-explorer/dist' : './';
-  // const base = command === 'build' ? 'https://10.195.20.28/demo-apps/satellite-explorer/dist/' : './';
+export default defineConfig(() => {
   return {
-    base,
-    plugins: [react()]
+    base: './',
+    plugins: [react()],
+    assetsInclude: ['**/*.csv', '**/*.txt', '**/*.json']
   };
 });
